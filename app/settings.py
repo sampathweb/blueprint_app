149225db4291519a6de56d8930e3a36ff9cd7888
Ramesh Sampath <ramesh@sampathweb.com>
2013-10-24 22:48:52 +0000

    update settings

--- app/settings.py
+++ app/settings.py
@@ -7,11 +7,11 @@ class Config(object):
 class ProdConfig(Config):
     SECRET_KEY = 'prod - tis is secret?'
     # SQL Alchemy settings
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db/app_prod.sqlite'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db/app_db.sqlite'
 
 class DevConfig(Config):
     SECRET_KEY = 'dev tis is secret?'
     DEBUG = True
     # SQL Alchemy settings
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db/app_dev.sqlite'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db/app_db.sqlite'
     SQLALCHEMY_ECHO = True
